b8bdd983c9ab7b8d6648a6068ac60275e99ba9a3
Paweł Włodarczyk <pawel.wlodarczyk@blueservices.pl>
2016-05-24 12:51:40 +0200

    fixed endpointInterface in TestServiceImpl

--- src/main/java/pl/blueservices/demo/ws/TestServiceImpl.java
+++ src/main/java/pl/blueservices/demo/ws/TestServiceImpl.java
@@ -3,7 +3,7 @@ package pl.blueservices.demo.ws;
 import javax.jws.WebService;
 
 @WebService(
-	endpointInterface = "pl.blueservices.ws.demo.TestService",
+	endpointInterface = "pl.blueservices.demo.ws.TestService",
 	targetNamespace = "blueservices.pl",
 	serviceName = "Test"
 )
